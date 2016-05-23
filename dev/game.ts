070fb6dcfdafacc6ec34877668282e3f83629b6c
Dimmy Marcelissen <dimwes@hotmail.com>
2016-05-23 11:48:19 +0200

    Witte bloed cel
    Speler toegevoegd en de keyboard input toegevoegd.

--- dev/game.ts
+++ dev/game.ts
@@ -1,7 +1,23 @@
 //Start the game
 class Game {
+     
+    private char1:WhiteBloodCell;
     
+    private utils:Utils;
+       
     constructor(){
         new Titlescreen();
+        this.char1 = new WhiteBloodCell(37, 39, 38, 40);
+        this.utils = new Utils();
+        
+        requestAnimationFrame(this.gameLoop.bind(this));
     }
+    
+    private gameLoop(){
+        this.char1.move();
+        
+        requestAnimationFrame(this.gameLoop.bind(this));
+    }
+    
+    
 }
